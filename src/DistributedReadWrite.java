08bf3f7ff1ddf003c785ad6e8fb88132184bc256
jbonot <jbonot@ualberta.ca>
2016-01-14 17:11:04 +0100

    Check that all words appended words are in final word string.

--- src/DistributedReadWrite.java
+++ src/DistributedReadWrite.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -11,6 +13,7 @@ public class DistributedReadWrite implements Runnable{
 	private static int WAIT_MAX = 5;
 	private static String WORDS[] = {"apple", "banana", "carrot", "date", "eggplant", "fig", "guava"};
 	private Node node;
+	private List<String> appended = new ArrayList<String>();
 	
 	public DistributedReadWrite(Node node) {
 		this.node = node;
@@ -44,17 +47,61 @@ public class DistributedReadWrite implements Runnable{
 			System.out.println("old string: " + wordString);
 			
 			// Append some random English word to the string.
-			if (wordString.length() == 0) {
-				wordString += WORDS[rand.nextInt(WORDS.length)];
-			} else {
-				wordString += " " + WORDS[rand.nextInt(WORDS.length)];
-			}
+			String newWord = WORDS[rand.nextInt(WORDS.length)];
+			if (wordString.length() != 0) {
+				wordString += " ";
+			} 
+
+			wordString += newWord;
+			this.appended.add(newWord);
 
 			this.node.sendWordStringToMaster(wordString);
 			System.out.println("new string: " + wordString);
 		}
 		
 		System.out.println("-------------------");
-		System.out.println("final string: " + this.node.getMasterString());
+		wordString = this.node.getMasterString();
+		System.out.println("final string: " + wordString);
+		
+		// Check that all appended words are in the final string.
+		String[] finalTokens = wordString.split(" ");
+		this.printStringList("words appended by this node:", this.appended);
+		
+		List<String> missing = new ArrayList<String>();
+		for (String word : this.appended) {
+			boolean found = false;
+			for (int i = 0; i < finalTokens.length; i++) {
+				if (word.equals(finalTokens[i])) {
+					found = true;
+					break;
+				}
+			}
+			
+			if (!found) {
+				missing.add(word);
+			}
+		}
+		
+		System.out.println("-------------------");
+		if (missing.size() == 0) {
+			System.out.println("All words are included in the final string.");
+		} else {
+			System.out.println("Some words are missing from the final string.");
+			this.printStringList("Words missing from final string:", missing);	
+		}
+
+	}
+	
+	/**
+	 * Prints a list of strings in the format: [caption]: [word] [word] [word]...
+	 * @param caption
+	 * @param list
+	 */
+	private void printStringList(String caption, List<String> list) {
+		System.out.print(caption);
+		for (String word : list) {
+			System.out.print(" " + word);
+		}
+		System.out.println();
 	}
 }
